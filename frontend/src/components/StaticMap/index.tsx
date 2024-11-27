41bcb1e4d5e966de72ab7a1631e4c196f8add83a
Jonathan Faria <jonathanfdsn@gmail.com>
2024-11-27 11:06:06 -0300

    StaticMap componente

---
+++ frontend/src/components/StaticMap/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { MapStyle } from "./style";
+import { api } from "../../utils/api";
+
+interface StaticMapProps {
+    origin: { latitude: number; longitude: number };
+    destination: { latitude: number; longitude: number };
+};
+
+export const StaticMap: React.FC<StaticMapProps> = ({ origin, destination }) => {
+    const [mapUrl, setMapUrl] = useState("");
+
+    api.post('/static-map', {
+        origin,
+        destination
+    }).then(response => {
+        setMapUrl(response.data.mapUrl);
+    }).catch(error => {
+        console.log(error);
+    });
+  
+    return (
+    <div>
+        {mapUrl ? (
+            <MapStyle
+            src={mapUrl}
+            alt={`Mapa mostrando a rota de (${origin.latitude}, ${origin.longitude}) para (${destination.latitude}, ${destination.longitude})`}
+            />
+        ) : (
+            <p>Carregando mapa...</p>
+        )}
+    </div>
+    );
+};
---
+++ frontend/src/components/StaticMap/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MapStyle = styled.img `
+    width: "100%";
+    height: "auto";
+    borderRadius: "8px"; 
+`;
